38382be9f2aeb39eaf846b3645b303f584581d56
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-08 21:18:42 +0100

    Test basic functionality of Client class
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,4 +1,11 @@
+import pytest
+try:
+    from unittest import mock
+except ImportError:
+    # python2
+    import mock
 
+from celery import Celery
 from myr.client import Client
 
 
@@ -7,3 +14,69 @@ class TestClient:
     def test_init(self):
         client = Client()
         assert client.task_registry is None
+
+    def test__send_discover(self):
+        with mock.patch.object(
+                Celery, 'send_task', return_value=None
+        ) as mock_method:
+            client = Client()
+            client._send_discover()
+            mock_method.assert_called_with(
+                *client.DISCOVERY_DEFAULT_ARGS,
+                **client.DISCOVERY_DEFAULT_KWARGS)
+            client._send_discover(args=())
+            mock_method.assert_called_with()
+            client._send_discover(args=(), kwargs={'u': 2})
+            mock_method.assert_called_with(**{'u': 2})
+
+    def test__process_discover(self):
+        client = Client()
+        discovered_tasks = {
+            'remote.procedure': {
+                'signature': {
+                    'args': ['a'],
+                    'defaults': None,
+                    'varkw': None,
+                    'varargs': None
+                },
+                'routing': {}
+            }
+        }
+        registry = client._process_discover(discovered_tasks)
+        assert len(registry) == 1
+
+        discovered_tasks['remote.task'] = discovered_tasks['remote.procedure']
+        registry = client._process_discover(discovered_tasks)
+        assert len(registry) == 2
+
+        registry['remote.task']['fn'](a=None)
+        with pytest.raises(TypeError):
+            registry['remote.procedure']['fn'](b=None)
+
+    def test_discover(self):
+
+        class AsyncResult:
+            def get(self):
+                return {
+                    'remote.procedure': {
+                        'signature': {
+                            'args': ['a'], 'defaults': None,
+                            'varkw': None, 'varargs': None
+                        },
+                        'routing': {}
+                    },
+                    'remote.task': {
+                        'signature': {
+                            'args': ['a', 'b'], 'defaults': None,
+                            'varkw': None, 'varargs': None
+                        },
+                        'routing': {}
+                    }
+                }
+
+        with mock.patch.object(
+            Celery, 'send_task', return_value=AsyncResult()
+        ):
+            client = Client()
+            client.discover()
+        assert len(client.task_registry) == 2
