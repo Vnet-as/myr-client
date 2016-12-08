ecbf3cf29ebbccf1328fdbfd7cb30939e879ba8c
Michal Kuffa <michal.kuffa@gmail.com>
2016-12-08 22:36:02 +0100

    Initial version of RemoteProcedureCaller
    RemoteProcedureCaller is doing lookup in client's task registry and for
    preparing the actual task calls.
    
    Signed-off-by: Michal Kuffa <michal.kuffa@gmail.com>

--- myr/client/__init__.py
+++ myr/client/__init__.py
@@ -17,8 +17,13 @@ class Client:
     DISCOVERY_DEFAULT_ARGS = ('myr.discovery.discover', )
     DISCOVERY_DEFAULT_KWARGS = {}
 
-    def __init__(self, app=None):
+    def __init__(
+        self,
+        app=None,
+        send_task_options=None
+    ):
         self.app = app or current_app
+        self.send_task_options = send_task_options
         self.task_registry = None
 
     def _send_discover(self, args=None, kwargs=None):
@@ -37,3 +42,53 @@ class Client:
     def discover(self):
         async_discovered = self._send_discover()
         self.task_registry = self._process_discover(async_discovered.get())
+
+    def call_task(self, name, args=None, kwargs=None, **options):
+        # validate given arguments
+        self.task_registry[name]['fn'](*args, **kwargs)
+        return self.app.send_task(name, args=args, kwargs=kwargs, **options)
+
+    @property
+    def rpc(self):
+        return RemoteProcedureCaller(
+            client=self, base=(), options=self.send_task_options)
+
+
+class RemoteProcedureCaller:
+
+    def __init__(self, client, base=(), options=None):
+        self.client = client
+        self.base = base
+        self.options = options or {}
+
+    def __getattr__(self, name):
+        new_base = self.base + (name, )
+        full_name = '.'.join(new_base)
+
+        # If there is task with such a name return it's
+        # preprepared task call
+        if full_name in self.client.task_registry:
+
+            # TODO: Copy __doc__ once it is available in the discovery
+            def fn(*args, **kwargs):
+                return self.client.call_task(
+                    name, args=args, kwargs=kwargs, **self.options)
+
+            fn.__name__ = name
+            return fn
+        else:
+            # If there is not task with such name:
+            # - check if there is task which name starts with our full name and
+            #   dot so on next __getattr__ call it eventualy can be called
+            # - or raise AttributeError
+            for task_name in self.client.task_registry:
+                if task_name.startswith(full_name + '.'):
+                    return self.__class__(
+                        client=self.client,
+                        base=new_base,
+                        options=self.options)
+            raise AttributeError(
+                "'%s' object has no attribute '%s'" % (self, name))
+
+    def __str__(self):
+        return '<%s base=%r>' % (self.__class__.__name__, self.base)
